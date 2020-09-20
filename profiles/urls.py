ae2b3a1c91f32c1f7fdda1d376ea8f0329ec48fc
samyakjain101 <samyakjain101@gmail.com>
2020-09-20 16:11:07 +0530

    Inbox View Done

--- profiles/urls.py
+++ profiles/urls.py
@@ -6,6 +6,6 @@ app_name = 'profiles'
 urlpatterns = [
     path('', IndexView.as_view(), name='index'),
     path('register/',UserRegistration.as_view(),name="register"),
-    path('login/', LoginView.as_view(), name='login'),
+    path('login/', LoginView.as_view(template_name='profiles/login.html'), name='login'),
     path('logout/', LogoutView.as_view(), name='logout'),
 ]
