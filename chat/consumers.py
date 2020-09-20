be168369fcfd868bddb8f2f14fd613f8390937b2
samyakjain101 <samyakjain101@gmail.com>
2020-09-20 18:41:18 +0530

    Chat Consumer Done

--- chat/consumers.py
+++ chat/consumers.py
@@ -1,17 +1,79 @@
 import json
-from channels.generic.websocket import WebsocketConsumer
+import uuid
+from channels.generic.websocket import AsyncWebsocketConsumer
+from .models import Chat
+from django.contrib.auth.models import User
+from profiles.models import Profile
 
-class ChatConsumer(WebsocketConsumer):
-    def connect(self):
-        self.accept()
+# docker run -p 6379:6379 -d redis:5
 
-    def disconnect(self, close_code):
-        pass
+class ChatConsumer(AsyncWebsocketConsumer):
+    async def connect(self):
+        self.room_name = self.scope['url_route']['kwargs']['room_name']
+        self.room_group_name = 'chat_%s' % self.room_name
 
-    def receive(self, text_data):
+        # Join room group
+        await self.channel_layer.group_add(
+            self.room_group_name,
+            self.channel_name
+        )
+
+        await self.accept()
+
+        await self.channel_layer.group_send(
+            self.room_group_name,
+            {
+                'type': 'chat_message',
+                'message': '{} is online'.format(self.scope['user'].username),
+            }
+        )
+
+    async def disconnect(self, close_code):
+        # Leave room group
+        await self.channel_layer.group_discard(
+            self.room_group_name,
+            self.channel_name
+        )
+        
+        await self.channel_layer.group_send(
+            self.room_group_name,
+            {
+                'type': 'chat_message',
+                'message': '{} is offline'.format(self.scope['user'].username)
+            }
+        )
+
+    # Receive message from WebSocket
+    async def receive(self, text_data):
         text_data_json = json.loads(text_data)
-        message = text_data_json['message']
+        if 'message' in text_data_json:
+            message = text_data_json['message']
 
-        self.send(text_data=json.dumps({
-            'message': message
-        }))
+            # Send message to room group
+            await self.channel_layer.group_send(
+                self.room_group_name,
+                {
+                    'type': 'chat_message',
+                    'message': message,
+                    'author': self.scope['user'].username,
+                }
+            )
+
+
+    # Receive message from room group
+    async def chat_message(self, event):
+        message = event['message']
+        if 'author' in event:
+            author = event['author']
+            # Send message to WebSocket
+            await self.send(text_data=json.dumps({
+                'message': message,
+                'author' : author,
+            }))
+        else:
+            # Send message to WebSocket
+            await self.send(text_data=json.dumps({
+                'message': message
+            }))
+
+        
--- chat/views.py
+++ chat/views.py
@@ -1,10 +1,15 @@
+import json
+import uuid
 from django.shortcuts import render
-from django.contrib.auth.mixins import LoginRequiredMixin
-from django.contrib.auth.models import User
 from django.views.generic import TemplateView
+from django.contrib.auth.models import User
+from django.core.exceptions import PermissionDenied
+from django.contrib.auth.decorators import login_required
+from django.core.exceptions import ObjectDoesNotExist
+from django.contrib.auth.mixins import LoginRequiredMixin
+from django.http import HttpResponse
 from .models import Chat
 
-# Create your views here.
 class InboxView(LoginRequiredMixin, TemplateView):
     template_name = "chat/inbox.html"
     def get_context_data(self, **kwargs):
@@ -13,5 +18,36 @@ class InboxView(LoginRequiredMixin, TemplateView):
         context = {
             'users': users,
             'chats': chats,
+            }
+        return context
+
+class RoomView(LoginRequiredMixin, TemplateView):
+    template_name = "chat/room.html"
+    def get_context_data(self, **kwargs):
+
+        # If room does not exist or id someone is trying to join a room of which he/she is not part of. Permission Denied
+        try:
+            room_name = uuid.UUID(self.kwargs['room_name']).hex
+        except ValueError:
+            raise PermissionDenied("Permission Denied")
+        
+        try:
+            chat = Chat.objects.get(chat_id = room_name, room = self.request.user.id)
+            for user in chat.room.all():
+                if user == self.request.user:
+                    pass
+                else:
+                    user2 = user
+        except ObjectDoesNotExist:
+            raise PermissionDenied("Permission Denied")
+        
+        contacts = Chat.objects.filter(room = self.request.user.id)
+
+        context = {
+            'room_name': self.kwargs['room_name'],
+            'current_user': self.request.user.username,
+            'user2': user2,
+            'chat' : chat,
+            'contacts' : contacts,
             }
         return context
