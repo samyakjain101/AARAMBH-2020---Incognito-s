7901f4e03372604073a769b0f9c7480e3d7ace93
Samarth Paliwal <ram.paliwal49@gmail.com>
2020-09-20 19:35:13 +0530

    blog - comments added, and create blog done

--- blog/admin.py
+++ blog/admin.py
@@ -1,5 +1,6 @@
 from django.contrib import admin
-from blog.models import Blog
+from blog.models import Blog, Comment
 # Register your models here.
 
 admin.site.register(Blog)
+admin.site.register(Comment)
