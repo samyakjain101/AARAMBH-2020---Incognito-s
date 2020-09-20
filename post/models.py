a1a7edd2cbff4b775eb9470ab8655679bb1a58bb
samyakjain101 <samyakjain101@gmail.com>
2020-09-20 22:35:04 +0530

    Post, Post Comment models created

--- post/models.py
+++ post/models.py
@@ -1,3 +1,26 @@
 from django.db import models
+from django.contrib.auth.models import User
+from django.utils import timezone
 
 # Create your models here.
+class Post(models.Model):
+    author = models.ForeignKey(User, on_delete = models.CASCADE)
+    content = models.TextField()
+    date = models.DateTimeField(default=timezone.now)
+    image = models.ImageField(upload_to='post')
+    likes = models.ManyToManyField(User, related_name = 'post_likes', blank=True)
+    comments = models.ManyToManyField(User, through='Comment', related_name = 'post_comments')
+
+    def __str__(self):
+        return str(self.author)
+
+class Comment(models.Model):
+    user = models.ForeignKey(User, on_delete=models.CASCADE)
+    post = models.ForeignKey(Post, on_delete=models.CASCADE)
+    comment = models.TextField()
+
+    def __str__(self):
+        return str(self.user)
+
+    
+
