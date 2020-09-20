7dc708dd0cf72756b7017ec65e06a17c22ca0d5b
Samarth Paliwal <ram.paliwal49@gmail.com>
2020-09-20 15:09:32 +0530

    login logout done

--- profiles/urls.py
+++ profiles/urls.py
@@ -1,9 +1,11 @@
 from django.urls import path
 from .views import *
-
+from django.contrib.auth.views import LoginView, LogoutView
 app_name = 'profiles'
 
 urlpatterns = [
     path('', IndexView.as_view(), name='index'),
     path('register/',UserRegistration.as_view(),name="register"),
+    path('login/', LoginView.as_view(), name='login'),
+    path('logout/', LogoutView.as_view(), name='logout'),
 ]
