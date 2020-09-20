c63d1ee084df0da35e4e0a4ac9fa20319ddf07a5
samyakjain101 <samyakjain101@gmail.com>
2020-09-20 16:42:19 +0530

    Chat Consumer setup done

--- CampusConnect/settings.py
+++ CampusConnect/settings.py
@@ -32,6 +32,8 @@ ALLOWED_HOSTS = ['*']
 # Application definition
 
 INSTALLED_APPS = [
+    'channels',
+    'chat',
     'django.contrib.admin',
     'django.contrib.auth',
     'django.contrib.contenttypes',
@@ -40,7 +42,6 @@ INSTALLED_APPS = [
     'django.contrib.staticfiles',
     'profiles',
     'blog',
-    'chat',
     'post',
 ]
 
@@ -73,7 +74,15 @@ TEMPLATES = [
 ]
 
 WSGI_APPLICATION = 'CampusConnect.wsgi.application'
-
+ASGI_APPLICATION = 'CampusConnect.routing.application'
+CHANNEL_LAYERS = {
+    'default': {
+        'BACKEND': 'channels_redis.core.RedisChannelLayer',
+        'CONFIG': {
+            "hosts": [('127.0.0.1', 6379)],
+        },
+    },
+}
 
 # Database
 # https://docs.djangoproject.com/en/3.0/ref/settings/#databases
