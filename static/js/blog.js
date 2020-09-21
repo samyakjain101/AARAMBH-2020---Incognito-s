7d187a45788bacdfe507a5941f1a5253695bb522
samyakjain101 <samyakjain101@gmail.com>
2020-09-21 19:27:49 +0530

    Blog Comment Ajaxified

--- static/js/blog.js
+++ static/js/blog.js
@@ -1,9 +1,9 @@
 $(document).ready(function () {
-    $('#comment-form').on('submit', function (event) {
+    $('.blog_area').on('submit', '#comment-form', function (event) {
         event.preventDefault()
         console.log('Comment')
         var form = $(this)
-        console.log(form.find("input[name=csrfmiddlewaretoken]"));
+        form.find('button').html('<div class="spinner-grow spinner-grow-sm" role="status"> <span class="sr-only">Loading...</span> </div>');
         $.ajax({
             type: "POST",
             url: form.find('#comment-input').attr('url'),
@@ -16,6 +16,15 @@ $(document).ready(function () {
         });
         function handleFormSuccess(json) {
             console.log(json.message);
+            $.ajax({
+                type: "GET",
+                url: window.location.pathname,
+                success: handleFormSuccess, 
+            });
+            function handleFormSuccess(data) {
+                var commentBody = $(data).find('.comments-area').html();
+                form.closest('.comments-area').html(commentBody);
+            }
         }
     });
 });
